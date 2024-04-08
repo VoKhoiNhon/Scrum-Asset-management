9797428343a6a5f4e9281381b0894558e87d7fa5
Võ Khôi Nhơn <vokhoinhon@gmail.com>
2024-04-08 09:55:42 +0700

    fixx bug duplicatecode when merge

---
+++ Asset_management_springboot/Asset_management/src/main/java/DACNPM/asset_management/controller/AssetController.java
@@ -0,0 +1,49 @@
+package DACNPM.asset_management.controller;
+
+import DACNPM.asset_management.model.Asset;
+import DACNPM.asset_management.service.AssetService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+
+import java.util.List;
+@Controller
+public class AssetController {
+
+    @Autowired
+    AssetService assetService;
+
+    @GetMapping("/home")
+    public String getAllAsset(Model model){
+        List<Asset> listAsset= assetService.getAllAssets();
+        model.addAttribute("listAsset",listAsset);
+        model.addAttribute("asset",new Asset());
+        return "index";
+    }
+//    @GetMapping("/test")
+//    public List<Asset> getAll(){
+//        return assetService.getAllAssets();
+//    }
+
+    @PostMapping("/addNewAsset")
+    public String addNewAsset(@ModelAttribute("asset") Asset asset){
+        assetService.addNewAsset(asset);
+        return "redirect:/home";
+    }
+
+    @PostMapping("updateAsset/{id}")
+    public String updateAsset(@PathVariable("id")int id,@ModelAttribute("asset") Asset asset) throws Exception {
+        assetService.updateAsset(id,asset);
+        return "redirect:/home";
+    }
+
+    @PostMapping("deleteAsset/{id}")
+    public String deleteAsset(@PathVariable("id")int id){
+        assetService.deleteAsset(id);
+        return "redirect:/home";
+    }
+
+}
---
+++ Asset_management_springboot/Asset_management/src/main/java/DACNPM/asset_management/repository/ListBorrowRepository.java
@@ -0,0 +1,7 @@
+package DACNPM.asset_management.repository;
+
+import DACNPM.asset_management.model.ListBorrow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ListBorrowRepository extends JpaRepository<ListBorrow,Integer> {
+}
